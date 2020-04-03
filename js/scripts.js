15a1087adce6cab6754faff3e613c8ae527b68e9
shonburgundy <seanbeloussow@yahoo.com>
2020-04-03 11:34:01 -0700

    add for loop to generateRange function

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,33 @@
 //Business Interface
-var convertSentence = function(num) {
-  var mrRobo = [3, 2, 1]
-  var roboReply = ["Won't you be my neighbor?", "Boop!", "Beep!"]
-  var numInput =
+// var convertSentence = function(num) {
+//   var mrRobo = [3, 2, 1]
+//   var roboReply = ["Won't you be my neighbor?", "Boop!", "Beep!"]
+//   var numInput =
   
-  var result = "";
+//   var result = "";
 
-  //for (var i = 0; i < mrRobo.length; i++) {
+//   //for (var i = 0; i < mrRobo.length; i++) {
     
+//   }
+    
+//   return result;
+//};
+
+  var generateRange = function(num) {
+  var displayArray = []
+  for (var i = 0; i  <= num; i++) {
+  displayArray.push(i);
+  } if ( i === 1) {
+    return  "Beep!"
   }
-    
-  return result;
-};
-
-
-
- // for (var i = 0; i  <= userInput; i++)
-
-
-
-
+  return displayArray;
+}
 
 $(document).ready(function() {
   $("form#formOne").submit(function(event) {
     event.preventDefault();
     var userInput = $("input#userInput").val();
-    $("#output").text(convertSentence(userInput));
-    console.log(result)
+    $("#output").text(generateRange(userInput));
+    console.log(userInput)
   });
 });

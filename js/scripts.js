54f616a21196ba668caf93ae429e47cfd9dabfe9
shonburgundy <seanbeloussow@yahoo.com>
2020-04-03 15:46:30 -0700

    add if branch for name input

--- js/scripts.js
+++ js/scripts.js
@@ -6,7 +6,10 @@
   var displayArray = [] //initializes empty array
   for (var i = 0; i  <= num; i++) { //for loop that increments index by 1 till <= user input
     //want to look at index value
-    if (i.toString().includes("3")) { // this is looking if the i contains a 3
+      if (i % 3 === 0) {
+      displayArray.push("Won't you be my neighbor," + name + "?")
+    }
+      else if (i.toString().includes("3")) { // this is looking if the i contains a 3
       //if its a 3 return wont.
       displayArray.push("Won't you be my neighbor?")
       //if 2 return boop
@@ -22,9 +25,6 @@
      //adds value of index to displayArray
   }
 
-  if (i % 3 === 0) {
-    displayArray.push("Won't you be my neighbor," + "#user-name" + "?")
-  }
   return displayArray; // the result
 }
 

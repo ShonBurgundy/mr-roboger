1930038f3d6e48dcd713f151aa95ba2089e7c876
shonburgundy <seanbeloussow@yahoo.com>
2020-04-03 10:30:12 -0700

    added more variables

--- js/scripts.js
+++ js/scripts.js
@@ -2,11 +2,11 @@
 var convertSentence = function(num) {
   var mrRobo = [3, 2, 1]
   var roboReply = ["Won't you be my neighbor?", "Boop!", "Beep!"]
-  
+  var numInput =
   
   var result = "";
 
-  for (var i = 0; i < mrRobo.length; i++) {
+  //for (var i = 0; i < mrRobo.length; i++) {
     
   }
     
@@ -26,7 +26,6 @@ $(document).ready(function() {
     event.preventDefault();
     var userInput = $("input#userInput").val();
     $("#output").text(convertSentence(userInput));
-    for (var i = 0; i  <= userInput; i++)
     console.log(result)
   });
 });

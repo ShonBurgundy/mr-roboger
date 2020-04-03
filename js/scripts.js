730e9f5d3bc0bc7ab2819675f795c0d86da3d44e
shonburgundy <seanbeloussow@yahoo.com>
2020-04-03 13:57:29 -0700

    else if branches completed

--- js/scripts.js
+++ js/scripts.js
@@ -4,22 +4,24 @@
   var displayArray = [] //initializes empty array
   for (var i = 0; i  <= num; i++) { //for loop that increments index by 1 till <= user input
     //want to look at index value
-    if (i.toString().includes("3")) {
+    if (i.toString().includes("3")) { // this is looking if the i contains a 3
       //if its a 3 return wont.
       displayArray.push("Won't you be my neighbor?")
       //if 2 return boop
-    } else if (i.toString().includes("2")) {
+    } else if (i.toString().includes("2")) {// this is looking if the i contains a 2
       displayArray.push("Boop!")
       //if 1 return beep
-    } else if (i.toString().includes("1")) {
+    } else if (i.toString().includes("1")) {// this is looking if the i contains a 1
       displayArray.push("Beep!")
+    } else {
+      displayArray.push(i);
     }
     //if its multiple digits from about highest value number take priority
-    displayArray.push(i); //adds value of index to displayArray
+     //adds value of index to displayArray
   }
   return displayArray; // the result
 }
-
+//UI
 $(document).ready(function() {
   $("form#formOne").submit(function(event) {
     event.preventDefault();
